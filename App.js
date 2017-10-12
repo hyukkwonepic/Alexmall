7ac9bb12a62a00814b10b07a74de18fc6bca4027
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2017-10-12 12:50:20 +0900

    Shop view included in a StackNavigator

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import { StackNavigator } from "react-navigation";
+import { Shop } from "./src/components/views";
+
+const Navigator = StackNavigator({
+    Shop: {
+        screen: Shop,
+        navigationOptions: {
+            title: "Shop"
+        }
+    }
+});
 
 class App extends Component {
     render() {
         return (
-            <View style={styles.wrapper}>
-                <Text>Hello</Text>
-            </View>
+            <Navigator />
         );
     }
 }
-
-const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
 
 export default App;
